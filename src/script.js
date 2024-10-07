06ca9d29352e7e9c8ff67f13df07b440a3471375
Maggie24-tech <107142910+Maggie24-tech@users.noreply.github.com>
2024-10-06 20:06:28 -0400

    added localstorage

--- src/script.js
+++ src/script.js
@@ -1,10 +1,27 @@
-function changeTheme() {
-  let body = document.querySelector("body");
-  if (body.classList.contains("dark")) {
-    body.classList.remove("dark");
-  } else {
-    body.classList.add("dark");
-  }
+let dark = localStorage.getItem("dark");
+const darkModeToggle = document.querySelector("#dark-mode-toggle");
+
+const enableDark = () => {
+  document.body.classList.add("dark");
+  localStorage.setItem("dark", "enabled");
+};
+
+const disableDark = () => {
+  document.body.classList.remove("dark");
+  localStorage.setItem("dark", null);
+};
+
+if (dark === "enabled") {
+  enableDark();
 }
-let themeButton = document.querySelector(".slider");
-themeButton.addEventListener("click", changeTheme);
+
+darkModeToggle.addEventListener("click", () => {
+  dark = localStorage.getItem("dark");
+  if (dark !== "enabled") {
+    enableDark();
+    console.log(dark);
+  } else {
+    console.log(dark);
+    disableDark();
+  }
+});
